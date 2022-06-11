cd829828646e871f77909fbbf397658d0b5bef65
Ilhom Ahmadjonov <ilhomuslim@gmail.com>
2022-06-11 19:25:47 +0000

    Revert "Implemented a report on the number of client growth"
    This reverts commit c886fc4735782c7a1b2984d735d3de720a64f8ec

--- app-server-side/src/main/java/uz/smart/service/ClientService.java
+++ app-server-side/src/main/java/uz/smart/service/ClientService.java
@@ -15,7 +15,6 @@ import uz.smart.entity.ListEntity;
 import uz.smart.exception.ResourceNotFoundException;
 import uz.smart.mapper.MapperUtil;
 import uz.smart.payload.ApiResponse;
-import uz.smart.projection.ClientGrowthCount;
 import uz.smart.repository.ClientRepository;
 import uz.smart.repository.ListRepository;
 import uz.smart.repository.OrderRepository;
@@ -96,9 +95,5 @@ public class ClientService {
             orderRepository.getActiveClientsCount(days),
             days
     );
-  }
-
-  public List<ClientGrowthCount> getClientCountByCreatedAt(Date begin, Date end) {
-   return repository.getClientCountByCreatedAt(begin, end);
   }
 }
